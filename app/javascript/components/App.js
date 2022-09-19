6da845de86cee25e1ca10ab4ca076c71c5aff4ce
Vaibhav D <dvaibhav350@gmail.com>
2022-09-19 16:15:29 +0530

    #Commit-2 remove unused code

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
-import Editor from './Editor';
-import './App.css';
 import RoutesPage from "./Router/route"
 
 const App = () => (
